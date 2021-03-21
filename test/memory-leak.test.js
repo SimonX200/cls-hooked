7f86cd40ca37bd9307355e94ffa90ca963833870
Aurel Balmosan <Aurel.Balmosan@weidmueller.com>
2021-03-21 17:28:17 +0100

    The performance problem fix also fixed the memory leak. A sleep is needed for the GC to run.

--- test/memory-leak.test.js
+++ test/memory-leak.test.js
@@ -2,6 +2,11 @@
 
 // Must be called with to fail: process.env.NODE_OPTIONS="--max-old-space-size=15"
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+
 async function t2(l) {
   if (l > 1) {
   }
@@ -16,6 +21,9 @@ describe('cls memory leak', function () {
     const ns = cls.createNamespace('abc');
     for (let i = 0; i < 100000; i++) {
       await ns.runPromise(t2);
+      if (i%1000===0) {
+        await sleep(1);
+      }
     }
   });
 });
